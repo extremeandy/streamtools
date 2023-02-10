9bd290bc5541fdb3423021f610d9d051370aa768
Andrew Harward <andrew@keyrock.eu>
2023-02-10 11:43:57 +1100

    docs: Fix missing attributes for docs for tokio-time features

--- src/lib.rs
+++ src/lib.rs
@@ -90,6 +90,7 @@ pub trait StreamTools: Stream {
     /// [`MissedTickBehavior`]: https://docs.rs/tokio/latest/tokio/time/enum.MissedTickBehavior.html
     /// [`Interval`]: https://docs.rs/tokio/latest/tokio/time/struct.Interval.html
     #[cfg(feature = "tokio-time")]
+    #[cfg_attr(docsrs, doc(cfg(feature = "tokio-time")))]
     fn sample_by_duration(
         self,
         duration: std::time::Duration,
@@ -106,6 +107,7 @@ pub trait StreamTools: Stream {
     ///
     /// This mirrors the behaviour of the [Sample](https://reactivex.io/documentation/operators/sample.html) operator in [ReactiveX](https://reactivex.io/).
     #[cfg(feature = "tokio-time")]
+    #[cfg_attr(docsrs, doc(cfg(feature = "tokio-time")))]
     fn sample_by_interval(
         self,
         interval: tokio::time::Interval,
